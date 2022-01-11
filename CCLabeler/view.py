779bbb1f37887b729afce80093270d51c881e2f5
victorjourne <journe.victor@gmail.com>
2022-01-11 09:31:12 +0100

    User is defined in label ping

--- CCLabeler/view.py
+++ CCLabeler/view.py
@@ -26,7 +26,7 @@ def disconnect(request):
 
 def ping(request):
     name = request.POST.get('user')
-    print('user %s want s to ping pong'%name)
+    print('user %s wants to pong'%name)
     player = Player(name)
     if player.pong:
         player.connect()
@@ -208,8 +208,7 @@ def table(request):
         username=name,
         cdata=cdata,
         form=form,
-        form_unlock_user=form_unlock_user,
-        submitted=False
+        form_unlock_user=form_unlock_user
     )
     return render(request, 'table.html', context)
 
@@ -320,7 +319,7 @@ def upload(request):
         if form.is_valid():
             msg = ''
             for f in request.FILES.getlist('file'):  # myfile is the name of your html file button
-                msg += handle_uploaded_file(f, str(f.name), str(request.POST['user'])) + "<br>"
+                msg += handle_uploaded_file(f, str(f.name), str(request.POST['user']))
             # Redirect to previous page
             # TODO: pass user/password to prevent from asking it again
             # return redirect(request.META['HTTP_REFERER'])
@@ -337,18 +336,15 @@ def handle_uploaded_file(file, filename, user):
         user_json = json.load(source)
     if imgid in user_json["data"]:
         return "The image %s exists in %s \n" % (filename, user)
-    image_path = os.path.join(utils.imgdir, filename)
-    if os.path.exists(image_path):
-        return "The image allready exists : %s \n" % (image_path)
     user_json["data"] += [imgid]
     with path_user_json.open("w", encoding="UTF-8") as target:
         json.dump(user_json, target)
 
     # Save image
-    with open(image_path, 'wb+') as destination:
+    with open(Path(utils.imgdir) / filename, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
 
     utils.init_image_jsons(imgid)
 
-    return "Image uploaded with success : " + image_path
+    return "Success"
--- js/global.js
+++ js/global.js
@@ -412,7 +412,7 @@ window.onload = function () {
     }, 30 * 1000);
 
     setInterval(function() {
-        ping();
+        ping(user);
     }, 1 * 1000 * 5);
 
 };
